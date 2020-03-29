a1a7f80b4bfd29ebd98839316a249d2e6d723fb4
shineunji <dmswlckato@gmail.com>
2020-03-29 21:15:31 +0900

    docs: array add

---
+++ inside-js/1/1-8.js
@@ -0,0 +1,30 @@
+// var colorArr = ['orange', 'blue', 'red', 'yellow'];
+
+// console.log(colorArr[0]); // 'orange'
+// console.log(colorArr[1]);
+
+// var emptyArr = []; // 빈 배열 
+// console.log(emptyArr[0]); // undefined
+
+// emptyArr[0] = 100;
+// emptyArr[3] = 'hello';
+// emptyArr[10] = true;
+
+// console.log(emptyArr);
+// console.log(emptyArr.length);
+
+// var arr = ['zero', 'one', 'two'];
+
+// arr.push('three'); // 배열 추가. push() 메서드 추가
+// console.log(arr); // ['zero', 'one', 'two', 'three']
+
+// arr.length =  5;
+
+// arr.push('four');
+// console.log(arr);
+
+
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+
